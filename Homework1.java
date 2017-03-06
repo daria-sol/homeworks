5afc294b97115fd14ab8e31d52701378e38a916b
luchchaya <luchchaya@gmail.com>
2017-03-06 18:03:06 +0200

    Update Homework1.java

--- Homework1.java
+++ Homework1.java
@@ -4,41 +4,48 @@ public class Homework1 {
 
     public static void main(String[] args) {
         //для целых чисел
-        int sum, dif, mul, div;
-        int a, b;
-        
-        a = 8;
-        b = 4;
-        
-        sum = a + b;
-        dif = a - b;
-        mul = a * b;
-        div = a / b;
+        int a = 8, b = 2;
         
         System.out.println("if a="+a+" "+"b="+b);
-        System.out.println("a+b=" + sum);
-        System.out.println("a-b=" + dif);
-        System.out.println("a*b=" + mul);
-        System.out.println("a/b=" + div);
-
-        //для дробных
-        double sumF, difF, mulF, divF;
-        double x, y;
+        System.out.println("a+b=" + sum(a,b));
+        System.out.println("a-b=" + dif(a,b));
+        System.out.println("a*b=" + mul(a,b));
+        System.out.println("a/b=" + div(a,b));
         
-        x =1.05;
-        y =0.3;
-        
-        sumF = x + y;
-        difF = x - y;
-        mulF = x * y;
-        divF = x / y;
-        
+        // для дробных чисел
+        double x=1.05,y=0.3;
         System.out.println("if x="+x+" "+"y="+y);
-        System.out.println("x+y=" + sumF);
-        System.out.println("x-y=" + difF);
-        System.out.println("x*y=" + mulF);
-        System.out.println("x/y=" + divF);
+        System.out.println("x+y=" + sumF(x,y));
+        System.out.println("x-y=" + difF(x,y));
+        System.out.println("x*y=" + mulF(x,y));
+        System.out.println("x/y=" + divF(x,y));
 
     }
-
+//соответсвенно методы для целых
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    public static int dif(int a, int b) {
+        return a-b;
+    }
+    public static int mul(int a, int b) {
+        return a*b;
+    }
+    public static int div(int a, int b) {
+        return a/b;
+    }
+    
+    //и для дробных
+    public static double sumF(double x, double y) {
+        return x+y;
+    }
+    public static double difF(double x, double y) {
+        return x-y;
+    }
+    public static double mulF(double x, double y) {
+        return x*y;
+    }
+    public static double divF(double x, double y) {
+        return x/y;
+    }
 }
